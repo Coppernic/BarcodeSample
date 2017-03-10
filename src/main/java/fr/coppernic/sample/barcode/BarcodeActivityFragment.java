62038a393111897cfb537bff094cad708ef9ccb7
Bastien Paul <bastien.paul@coppernic.fr>
2017-03-10 10:37:32 +0100

    [DEV] first commit

---
+++ src/main/java/fr/coppernic/sample/barcode/BarcodeActivityFragment.java
@@ -0,0 +1,22 @@
+package fr.coppernic.sample.barcode;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class BarcodeActivityFragment extends Fragment {
+
+	public BarcodeActivityFragment() {
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	                         Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.fragment_barcode, container, false);
+	}
+}
